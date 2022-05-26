55e0401240166b6c558faac048b1b918a6dd21ae
Maddy <madisonschaaf48@gmail.com>
2022-05-25 18:46:52 -0600

    extra comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 
-//import all of the api routes from /api/index.js (no need for index.js though since its implied)
 const apiRoutes = require('./api')
 
-//add prefix of /api to all of the api routes imported from the api directory
 router.use('/api', apiRoutes)
 
 
